750ea5dfce4afbc77ae21859a069eeec45059a40
Jungwon Shin <Jungwon.shin98@gmail.com>
2021-12-16 22:11:16 -0500

    image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return ( <
         Layout pageTitle = "Home Page" >
         <
-        p > I 'm making this by following the Gatsby Tutorial.</p> <
-        /Layout>
+        p > I 'm making this by following the Gatsby Tutorial.</p>  <
+        StaticImage alt = "the universe"
+        src = "../images/unsplash.jpg" /
+        >
+        < /
+        Layout >
     )
 }
 export default IndexPage
